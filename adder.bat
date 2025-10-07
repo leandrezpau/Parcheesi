78c859fbd1cbb0b230a03d784ff6e7dcbee14884
leandrezgo <leandrezgo@esat-libertad.com>
2025-10-07 11:02:16 +0200

    cosasborradas

---
+++ adder.bat
@@ -0,0 +1,12 @@
+git add main.cc
+git add parcheesi.cc
+git add piece.cc
+git add player.cc
+
+git add main.h
+git add parcheesi.h
+git add piece.h
+git add player.h
+
+git add compilador.bat
+git add adder.bat
